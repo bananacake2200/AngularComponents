80b856f2d0868c2554248efcb7df56ec531ac07a
bananacake2200 <marlyncureg2@gmail.com>
2024-09-19 18:42:13 +0800

    12th commit: wordreverser

---
+++ src/app/Components/wordreverser/wordreverser.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wordreverser',
+  templateUrl: './wordreverser.component.html',
+  styleUrl: './wordreverser.component.css'
+})
+export class WordreverserComponent {
+  inputWord: string = '';  
+  reversedWord: string = '';  
+
+  
+  reverseWord() {
+    this.reversedWord = this.inputWord.split('').reverse().join('');
+  }
+}
