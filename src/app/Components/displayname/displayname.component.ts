65a10dba22214634dd53272526128bee904e9953
bananacake2200 <marlyncureg2@gmail.com>
2024-09-19 16:05:26 +0800

    3rd commit: displayname

---
+++ src/app/Components/displayname/displayname.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displayname',
+  templateUrl: './displayname.component.html',
+  styleUrl: './displayname.component.css'
+})
+export class DisplaynameComponent {
+  userName: string = '';  
+  displayedName: string = ''; 
+
+  displayUserName() {
+    this.displayedName = this.userName;
+  }
+
+}
