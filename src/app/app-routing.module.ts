e333e6b8c08a8b64284789da79ca7dcfde016b7c
bananacake2200 <marlyncureg2@gmail.com>
2024-09-19 15:45:28 +0800

    first commt:displayhelloworld

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './Components/landingpage/landingpage.component';
+import { DisplayhelloworldComponent } from './Components/displayhelloworld/displayhelloworld.component';
+
+const routes: Routes = [ 
+
+  {path: "", component: LandingpageComponent},
+  {path: "displayhelloworld", component: DisplayhelloworldComponent},
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
