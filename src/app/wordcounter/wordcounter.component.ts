e338bb47f0709c79749e5b0b5c5f1cab4929e052
bananacake2200 <marlyncureg2@gmail.com>
2024-09-20 09:47:27 +0800

    26th commit: wordcouunter

---
+++ src/app/wordcounter/wordcounter.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wordcounter',
+  templateUrl: './wordcounter.component.html',
+  styleUrl: './wordcounter.component.css'
+})
+export class WordcounterComponent {
+  inputText: string = '';
+  wordCount: number = 0;   
+
+  
+  countWords(): void {
+    if (this.inputText.trim()) {
+     
+      this.wordCount = this.inputText.trim().split(/\s+/).length;
+    } else {
+      this.wordCount = 0; 
+    }
+  }
+}
